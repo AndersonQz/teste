ab89c8739b2d37e94e69fc78b6fbb5184651671b
AndersonQz <85910811+AndersonQz@users.noreply.github.com>
2021-06-15 09:21:03 -0300

    Add files via upload
    teste

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="querecebe.php" method="post">
+Nome: <input type="text" name="nome"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
